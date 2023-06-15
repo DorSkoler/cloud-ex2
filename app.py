bdc7ffce499613b76c711106fed3f083d1b77835
Yon Garber <43474593+yongarber@users.noreply.github.com>
2023-06-15 20:10:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -258,10 +258,15 @@ def ssh_and_run_code(instance_ip, KeyName):
                 print(stdout.read().decode())
                 print(stderr.read().decode())
 
-        except paramiko.AuthenticationException:
+        except AuthenticationException:
             print("Authentication failed. Please check your credentials.")
-        except paramiko.SSHException as e:
+            break  # Authentication failed, break out of the loop
+        except SSHException as e:
             print(f"SSH connection failed: {str(e)}")
+            print("Retrying in 5 seconds...")
+            time.sleep(5)  # Wait for 5 seconds before retrying
+        except NoValidConnectionsError as e:
+            print(f"Unable to establish SSH connection: {str(e)}")
             print("Retrying in 5 seconds...")
             time.sleep(5)  # Wait for 5 seconds before retrying
 
